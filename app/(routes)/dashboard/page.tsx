41c54a23834fe24316b7d8148bdda669a36274dc
minkhantko-0 <minkhant5771@gmail.com>
2024-09-16 11:20:39 +0630

    create user and serach user by emails convex funcs

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,11 +1,34 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
 import {
   LogoutLink,
   useKindeBrowserClient,
 } from "@kinde-oss/kinde-auth-nextjs";
+import { useMutation, useQuery } from "convex/react";
+import { useEffect } from "react";
 
 function Dashboard() {
+  const { user, isLoading } = useKindeBrowserClient();
+
+  const users = useQuery(api.user.getByEmail, {
+    email: user?.email as string,
+  });
+
+  const createUser = useMutation(api.user.create);
+
+  useEffect(() => {
+    if (user && !isLoading) {
+      if (users === null || users?.length === 0) {
+        createUser({
+          name: user.given_name!,
+          email: user.email!,
+          image: user.picture ? user.picture : undefined,
+        }).then((res) => console.log(res));
+      }
+    }
+  }, [user, users]);
+
   return (
     <div>
       Dashboard
---
+++ convex/user.ts
@@ -0,0 +1,25 @@
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+export const getByEmail = query({
+  args: {
+    email: v.string(),
+  },
+  handler: async (ctx, args) => {
+    return await ctx.db
+      .query("user")
+      .filter((q) => q.eq(q.field("email"), args.email))
+      .collect();
+  },
+});
+
+export const create = mutation({
+  args: {
+    name: v.string(),
+    email: v.string(),
+    image: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    return await ctx.db.insert("user", args);
+  },
+});
